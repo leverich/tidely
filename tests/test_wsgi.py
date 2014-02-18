75a4439ab32d17b8e291c4c1b3368741418efc4f
Jacob Leverich <leverich@gmail.com>
2014-02-17 20:47:48 -0800

    Added testcases for WSGI server. Moved unittests to tests/.

---
+++ tests/test_wsgi.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from webtest import TestApp
+import unittest
+import web
+
+import main
+
+class TestTidelyWSGI(unittest.TestCase):
+    def setUp(self):
+        self.app = TestApp(main.application)
+
+    def test_tidely_index(self):
+        response = self.app.get('/')
+        self.assertEquals(response.status_int, 200)
+        self.assertTrue('box' in response)
+
+    def test_tidely_site(self):
+        response = self.app.get('/site?current_site=San+Francisco+Bay+Entrance')
+        self.assertEquals(response.status_int, 200)
+        self.assertTrue('tidetable' in response)
+
+    def test_tidely_graph(self):
+        response = self.app.get('/graph?site=San+Francisco+Bay+Entrance')
+        self.assertEquals(response.status_int, 200)
+        self.assertTrue(response.content_length > 0)
+        self.assertEquals(response.content_type, 'image/png')
+
+if __name__ == '__main__':
+    unittest.main()
